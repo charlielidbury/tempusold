33898b5c0715687430337906fce192cb3393b0cd
Charlie Lidbury <charlie.lidbury@icloud.com>
2017-12-21 23:17:36 +0000

    Automated Commit

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php echo phpinfo(); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>This is a test</title>
+  </head>
+  <body>
+    <h1>This is the second test on this page.</h1>
+  </body>
+</html>
