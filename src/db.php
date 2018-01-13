b7ea1eb2b175f419ae6332ae2efe1c807bb9559d
Charlie Lidbury <charlie.lidbury@icloud.com>
2018-01-13 09:15:10 +0000

    Added getCell function which just searches the database for a specific cell speicifed by the arguments

--- src/db.php
+++ src/db.php
@@ -12,6 +12,15 @@ if ($conn->connect_error)
 
 function query2Table($conn, $query, $format="", $arg="")
 {
+	/*
+	PRINTS HTML REPRESENTATION OF A QUERY
+
+	$conn <= connection object
+	$query <= query with ?'s as variable names ("SELECT * FROM `employee` WHERE `name` = ?")
+	$format <= data type of $arg (s=string, i=int, d=double, b=blob)
+	$arg <= arg to be passed to the query in place of ?
+	*/
+
 	$stmt = $conn->prepare($query);
 
 	if (!$stmt) die ("Statement failed to prepare: " . $mysqli->error);
@@ -42,6 +51,51 @@ function query2Table($conn, $query, $format="", $arg="")
 	echo "</table>";
 
 	$stmt->close();
+}
+
+function getCell($conn, $column, $table, $key, $value)
+{
+	/*
+	GETS A CELL FROM A table
+
+	$conn <= connection object
+	$table <= name of table data is to be extracted from
+	$column <= which field the data is stored in
+	$key <= field for data to be recognised with (WHERE $key = "Charlie")
+	$key <= value for data to be recognised with (WHERE "name" = $value)
+
+	returns => the single value from the table
+	*/
+
+	$stmt = $conn->prepare("SELECT `$column` FROM `$table` WHERE `$key` = ?");
+
+	if (!$stmt) die ("Statement failed to prepare: " . $conn->error);
+
+	// makes the format string 'sssi' for instance
+	$conversion = array(
+		"integer" => "i",
+		"double"  => "d",
+		"string"  => "s"
+	);
+
+	$format = $conversion[gettype($value)];
+
+	// binds things together
+	$stmt->bind_param($format, $value);
+	// execute query
+	$stmt->execute();
+
+	// get result
+	$result = $stmt->get_result();
+
+	// gets result
+	if (!( $row = $result->fetch_row() ))
+		die("No results!");
+
+	$stmt->close();
+
+	return $row[0];
+
 }
 
 ?>
