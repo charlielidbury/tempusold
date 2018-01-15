f91f2cdc161af871ab148264698a34fb6badd057
Charlie Lidbury <charlie.lidbury@icloud.com>
2018-01-15 17:50:59 +0000

    Fixed bugs: 	~Couldn't change user details because it was looking for $_POST['username'] which didn't exist. Was supposed to be $_POST['name']

--- home/members/change_details.php
+++ home/members/change_details.php
@@ -21,8 +21,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') // update has been pressed
 	$errors = [];
 
 	// CHECK: NAME HAS NON WHITESPACES
-	if (0 === preg_match("/\S+/", $_POST['username']))
-	$errors[] = "Must enter a username.";
+	if (0 === preg_match("/\S+/", $_POST['name']))
+		$errors[] = "Must enter a username.";
 
 	// CHECK: NAME DOESN'T CONTAIN SPACES
 	if (0 !== preg_match('/\s/', $_POST['name']))
