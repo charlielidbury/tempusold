0907890e294a65f501809b889d43b7cb32cc0d46
Charlie Lidbury <charlie.lidbury@icloud.com>
2018-01-16 21:16:10 +0000

    Added pages: 	~create_payment.php 	~view_payment.php which just vardump their get parameters view_session adjusted to include info about the users who attended the session

---
+++ home/payments/view_payment.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Tempus - View Payment</title>
+	</head>
+	<body>
+		<pre><?= var_dump($_GET) ?></pre>
+	</body>
+</html>
---
+++ home/sessions/view_session.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+
+include "{$_SERVER['DOCUMENT_ROOT']}/src/db.php";
+
+// makes sure only logged on users past this point
+if (!isset($_SESSION['user']))
+	header("Location: http://{$_SERVER['HTTP_HOST']}");
+
+// makes sure only people with correct perms can see the details
+if (!hasPerms($conn, "sessions", 1))
+	header("Location: http://{$_SERVER['HTTP_HOST']}/permission_denied.php");
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Tempus - View Session</title>
+		<link rel="stylesheet" href="/css/style.css"/>
+	</head>
+	<body>
+		<pre><?php //die(var_export($_SESSION['user_data'], true)) ?></pre>
+		<h1><a href="/">Tempus</a></h1>
+		<h2><a href="/home">Home</a></h2>
+		<h3><a href="/home/team">Team</a></h3>
+		<h3><a href="/home/team/view_session.php?session=<?= $_GET['session']; ?>">View User</a></h3>
+		<p>Profile details:</p>
+		<?php
+		// SESSION DETAILS
+		// 2018-01-16 => 16/01/18
+		$date = sprintf("%s/%s/%s",
+			substr($_GET['session'], 8, 2), // day
+			substr($_GET['session'], 5, 2), // month
+			substr($_GET['session'], 2, 2) // year
+		);
+		echo row2HTML($conn, "view_session", "Date", $date);
+
+		// CONTAINED SHIFT DETAILS
+		echo table2HTML($conn, "CALL sessionShifts(?)", "s", $_GET['session']);
+
+		?>
+	</body>
+</html>
