51e9ff8626eeaaaab5d531d4d972d318dfa92b27
Charlie Lidbury <charlie.lidbury@icloud.com>
2018-03-29 23:44:23 +0100

    Added clock.php

---
+++ home/clock.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+// redirects users who aren't logged in
+if (!isset($_SESSION['user']))
+	header("Location: http://{$_SERVER['HTTP_HOST']}/login.php?redirect={$_SERVER['REQUEST_URI']}");
+
+include "{$_SERVER['DOCUMENT_ROOT']}/src/db.php";
+
+q($conn, "CALL toggleClock(?)", ['args'=>$_SESSION['user']]);
+
+if (isset($_GET['redirect']))
+	header("Location: {$_GET['redirect']}");
+
+?>
